dc2163ecded119917c5d9ecc8484e6685531bc19
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-06-03 19:26:38 +0600

    Add files via upload

--- 6.23/6.23/Program.cs
+++ 6.23/6.23/Program.cs
@@ -25,14 +25,55 @@ namespace l6t23
         {
             ArrayList arr = new ArrayList() { "ч", 1, 2, 3, "q", true, false, "й", 1.11, 2.2, 1d, 2d, 17, 17.0, 123, "int32", false, true };
             /* Добавьте свой код ниже */
-            List<int> celnum = new List<int>();
-            List<double> dronum = new List<double>();
-            List<string> words = new List<string>();
-            List<bool> logic = new List<bool>();
-            int b;
-            foreach(ArrayList a in arr)
+            List<int> A = new List<int>();
+            List<double> B = new List<double>();
+            List<string> C = new List<string>();
+            List<bool> D = new List<bool>();
+
+            for (int i = 0; i < arr.Count; i++)
             {
-                
+                string arr1 = Convert.ToString(arr[i]);
+                if (Double.TryParse(arr1, out double e) == true)
+                {
+                    double b = Double.Parse(arr1);
+                    if(b - Math.Floor(b)==0)
+                    {
+                        int a = Convert.ToInt32(b);
+                        A.Add(a);
+                    }
+                    else
+                    {
+                        B.Add(b);
+                    }
+                }
+                else if (arr1=="True" || arr1 == "False")
+                {
+                    bool d = Convert.ToBoolean(arr[i]);
+                    D.Add(d);
+                }
+                else
+                {
+                    C.Add(arr1);
+                }
+            }
+            foreach(int a in A)
+            {
+                Console.Write($"{a}\t");
+            }
+            Console.WriteLine();
+            foreach (double b in B)
+            {
+                Console.Write($"{b}\t");
+            }
+            Console.WriteLine();
+            foreach (string c in C)
+            {
+                Console.Write($"{c}\t");
+            }
+            Console.WriteLine();
+            foreach (bool d in D)
+            {
+                Console.Write($"{d}\t");
             }
         }
     }
